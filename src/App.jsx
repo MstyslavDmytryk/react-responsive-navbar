b997dbcd3be6a4b1edb7dcf4e4f680aa81272a00
MstyslavDmytryk <mstyslavdmytryk@gmail.com>
2023-01-31 23:54:03 +0200

    css styles and some markup

---
+++ src/App.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <div className="app">
+      <nav>
+        <div className="burger-btn">
+          <span />
+        </div>
+      </nav>
+      <main>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
+          eveniet, doloribus odio odit provident facilis iste quas ad quos
+          numquam! Accusamus eaque molestiae ipsa accusantium animi culpa
+          tempore provident dolores officiis. Autem assumenda reiciendis, omnis
+          maxime expedita corrupti! Quis consequatur ex eaque quasi voluptatum
+          laudantium iure fuga incidunt, qui laboriosam corrupti, ut ipsam in
+          vero veniam, eum corporis enim adipisci recusandae! Vero quam ipsam,
+          ea voluptatem assumenda laudantium, cupiditate reiciendis in sed ullam
+          modi at consequuntur illo tenetur maiores totam? Expedita, quasi quos?
+          Voluptas dolor porro illo optio eaque, atque distinctio perspiciatis
+          commodi beatae culpa corporis iusto nesciunt alias natus ea deleniti
+          non? Suscipit, soluta? Ea corrupti maiores consectetur dolores commodi
+          rem quaerat quia optio omnis. Reprehenderit eveniet aliquid distinctio
+          possimus cum in beatae qui animi, dolore quia quam? Natus aperiam
+          quaerat nisi modi repellat eos provident expedita alias eaque ipsa,
+          necessitatibus, voluptatem, ad ducimus quo voluptas tenetur voluptate
+          mollitia vel? Minima neque, laudantium vitae expedita quibusdam
+          voluptatem non possimus earum, eius debitis obcaecati atque ipsam amet
+          corrupti delectus hic dignissimos dicta repellat ullam consequuntur
+          tenetur autem fugit recusandae eveniet? Laudantium aliquid esse,
+          deleniti est suscipit ea sapiente possimus aperiam tenetur voluptatem
+          recusandae deserunt itaque! Aliquid et nihil maxime, a laudantium eius
+          vel quis sint odio magni earum assumenda veniam reprehenderit animi
+          cum aspernatur eos doloribus explicabo nulla accusamus dolorem! Fugit
+          quae impedit sequi, esse non nostrum neque corporis quidem, veniam,
+          qui temporibus? Cumque iusto voluptatum, odio eos molestias, in
+          aperiam labore, unde reiciendis excepturi laudantium assumenda nihil
+          ipsa animi cupiditate. Reiciendis iure voluptatem fuga quidem, ipsam
+          quia, voluptas ullam ratione sunt facere placeat ducimus a enim sequi
+          accusamus sit odit illum. Tempora amet, accusantium beatae, atque
+          repudiandae, assumenda omnis sapiente unde iusto magni quasi error
+          placeat nemo pariatur. Et consequuntur rem voluptatibus qui eius vitae
+          unde quod excepturi ut.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
+          eveniet, doloribus odio odit provident facilis iste quas ad quos
+          numquam! Accusamus eaque molestiae ipsa accusantium animi culpa
+          tempore provident dolores officiis. Autem assumenda reiciendis, omnis
+          maxime expedita corrupti! Quis consequatur ex eaque quasi voluptatum
+          laudantium iure fuga incidunt, qui laboriosam corrupti, ut ipsam in
+          vero veniam, eum corporis enim adipisci recusandae! Vero quam ipsam,
+          ea voluptatem assumenda laudantium, cupiditate reiciendis in sed ullam
+          modi at consequuntur illo tenetur maiores totam? Expedita, quasi quos?
+          Voluptas dolor porro illo optio eaque, atque distinctio perspiciatis
+          commodi beatae culpa corporis iusto nesciunt alias natus ea deleniti
+          non? Suscipit, soluta? Ea corrupti maiores consectetur dolores commodi
+          rem quaerat quia optio omnis. Reprehenderit eveniet aliquid distinctio
+          possimus cum in beatae qui animi, dolore quia quam? Natus aperiam
+          quaerat nisi modi repellat eos provident expedita alias eaque ipsa,
+          necessitatibus, voluptatem, ad ducimus quo voluptas tenetur voluptate
+          mollitia vel? Minima neque, laudantium vitae expedita quibusdam
+          voluptatem non possimus earum, eius debitis obcaecati atque ipsam amet
+          corrupti delectus hic dignissimos dicta repellat ullam consequuntur
+          tenetur autem fugit recusandae eveniet? Laudantium aliquid esse,
+          deleniti est suscipit ea sapiente possimus aperiam tenetur voluptatem
+          recusandae deserunt itaque! Aliquid et nihil maxime, a laudantium eius
+          vel quis sint odio magni earum assumenda veniam reprehenderit animi
+          cum aspernatur eos doloribus explicabo nulla accusamus dolorem! Fugit
+          quae impedit sequi, esse non nostrum neque corporis quidem, veniam,
+          qui temporibus? Cumque iusto voluptatum, odio eos molestias, in
+          aperiam labore, unde reiciendis excepturi laudantium assumenda nihil
+          ipsa animi cupiditate. Reiciendis iure voluptatem fuga quidem, ipsam
+          quia, voluptas ullam ratione sunt facere placeat ducimus a enim sequi
+          accusamus sit odit illum. Tempora amet, accusantium beatae, atque
+          repudiandae, assumenda omnis sapiente unde iusto magni quasi error
+          placeat nemo pariatur. Et consequuntur rem voluptatibus qui eius vitae
+          unde quod excepturi ut.
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit
+          eveniet, doloribus odio odit provident facilis iste quas ad quos
+          numquam! Accusamus eaque molestiae ipsa accusantium animi culpa
+          tempore provident dolores officiis. Autem assumenda reiciendis, omnis
+          maxime expedita corrupti! Quis consequatur ex eaque quasi voluptatum
+          laudantium iure fuga incidunt, qui laboriosam corrupti, ut ipsam in
+          vero veniam, eum corporis enim adipisci recusandae! Vero quam ipsam,
+          ea voluptatem assumenda laudantium, cupiditate reiciendis in sed ullam
+          modi at consequuntur illo tenetur maiores totam? Expedita, quasi quos?
+          Voluptas dolor porro illo optio eaque, atque distinctio perspiciatis
+          commodi beatae culpa corporis iusto nesciunt alias natus ea deleniti
+          non? Suscipit, soluta? Ea corrupti maiores consectetur dolores commodi
+          rem quaerat quia optio omnis. Reprehenderit eveniet aliquid distinctio
+          possimus cum in beatae qui animi, dolore quia quam? Natus aperiam
+          quaerat nisi modi repellat eos provident expedita alias eaque ipsa,
+          necessitatibus, voluptatem, ad ducimus quo voluptas tenetur voluptate
+          mollitia vel? Minima neque, laudantium vitae expedita quibusdam
+          voluptatem non possimus earum, eius debitis obcaecati atque ipsam amet
+          corrupti delectus hic dignissimos dicta repellat ullam consequuntur
+          tenetur autem fugit recusandae eveniet? Laudantium aliquid esse,
+          deleniti est suscipit ea sapiente possimus aperiam tenetur voluptatem
+          recusandae deserunt itaque! Aliquid et nihil maxime, a laudantium eius
+          vel quis sint odio magni earum assumenda veniam reprehenderit animi
+          cum aspernatur eos doloribus explicabo nulla accusamus dolorem! Fugit
+          quae impedit sequi, esse non nostrum neque corporis quidem, veniam,
+          qui temporibus? Cumque iusto voluptatum, odio eos molestias, in
+          aperiam labore, unde reiciendis excepturi laudantium assumenda nihil
+          ipsa animi cupiditate. Reiciendis iure voluptatem fuga quidem, ipsam
+          quia, voluptas ullam ratione sunt facere placeat ducimus a enim sequi
+          accusamus sit odit illum. Tempora amet, accusantium beatae, atque
+          repudiandae, assumenda omnis sapiente unde iusto magni quasi error
+          placeat nemo pariatur. Et consequuntur rem voluptatibus qui eius vitae
+          unde quod excepturi ut.
+        </p>
+      </main>
+    </div>
+  );
+};
+
+export default App;
